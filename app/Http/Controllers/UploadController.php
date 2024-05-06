f8cf1cff3179e63e2449930cb1e8c24c10d77acc
w.kong <work.kongwei@gmail.com>
2024-05-06 15:08:28 +0800

    fix

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -3,62 +3,51 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Support\Facades\Storage;
+use Intervention\Image\ImageManagerStatic as Image;
 
 class UploadController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        
-    }
-
     public function upload(Request $request)
     {
         $error = false;
 
-        if ($request->property_image) {
+        if ($request->images) {
             $allowedFileExtension = ['jpg', 'png'];
 
-            foreach ($request->property_image as $key => $value) {
+            foreach ($request->images as $key => $value) {
                 if (preg_match('/^data:image\/(\w+);base64,/', $value['image'])) {
                     $data = substr($value['image'], strpos($value['image'], ',') + 1);
                     $extension = explode('/', mime_content_type($value['image']))[1];
                     $check = in_array($extension, $allowedFileExtension);
-
+                    
                     if ($check) {
                         $image_name = md5(rand(1000, 10000));
                         $extension = strtolower($extension);
                         $image_full_name = $image_name . '.' . $extension;
-                        $uploade_path = 'uploads/images/' . $request->module . '/' . $request->id . '/';
+                        $uploade_path = 'public/uploads/images/' . $request->module . '/' . $request->id . '/';
                         $image_url = $uploade_path . $image_full_name;
-                        $image[$key]['url'] = $image_url;
-                        $image[$key]['default'] = $value['default'];
+
+                        $storage_path = 'storage/uploads/images/' . $request->module . '/' . $request->id . '/';
+                        $storage_url = $storage_path . $image_full_name;
+
+                        $image[$key]['url'] = $storage_url;
+                        $image[$key]['default'] = $value['default'] ?? 0;
 
                         $data = base64_decode($data);
+
+                        // $data = $this->resize($data);
+
                         Storage::disk('local')->put($image_url, $data);
                     } else {
                         $error = true;
                         break;
                     }
+                }else{
+                    $image = [];
                 }
             }
-
-            // foreach ($files as $file) {
-            // $filename = $file->getClientOriginalName();
-            // $extension = $file->getClientOriginalExtension();
-            // $compressed = \Image::make($file)->encode('png', 90);
-            // $upload_path = $request->module . '/' . $request->id . '/';
-            // $file->store('image/' . $upload_path, 'public');
-            // }
         } else {
             $error = true;
         }
@@ -75,5 +64,9 @@ class UploadController extends Controller
             'data' => $image,
             'message' => 'Upload success',
         ], Response::HTTP_OK);
+    }
+
+    private function resize($file){
+        return Image::make($file)->resize(500, 500, function($constraint){ $constraint->aspectRatio(); $constraint->upsize(); })->stream('jpg', 100);
     }
 }
--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -32,9 +32,9 @@ class Kernel extends HttpKernel
         'web' => [
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-            \Illuminate\Session\Middleware\StartSession::class,
+            // \Illuminate\Session\Middleware\StartSession::class,
             // \Illuminate\Session\Middleware\AuthenticateSession::class,
-            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            // \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
@@ -54,11 +54,11 @@ class Kernel extends HttpKernel
      * @var array<string, class-string|string>
      */
     protected $routeMiddleware = [
-        'auth' => \App\Http\Middleware\Authenticate::class,
-        'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        // 'auth' => \App\Http\Middleware\Authenticate::class,
+        // 'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
-        'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+        // 'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
